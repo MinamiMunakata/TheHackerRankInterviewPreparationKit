1353fea9acee675b19e111a3352e5e0421f1a2ac
Minami Munakata <june28munazo@gmail.com>
2019-01-29 14:32:17 -0800

    Solve 'SockMerchant'

---
+++ src/WarmUpChallenges/SockMerchant.java
@@ -0,0 +1,62 @@
+package WarmUpChallenges;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * Created by minami_munakata on 2019-01-29.
+ */
+public class SockMerchant {
+
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar) {
+        Map<Integer, Integer> socks = new HashMap<>();
+        for (int color: ar) {
+            if (socks.containsKey(color)) {
+                socks.put(color, socks.get(color) + 1);
+            } else {
+                socks.put(color, 1);
+            }
+        }
+        int count = 0;
+        for (int color: socks.keySet()) {
+            count += socks.get(color) / 2;
+        }
+        return count;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[n];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+        System.out.println(result);
+    }
+
+}
